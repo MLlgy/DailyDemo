bd3fe95206ad7fe282a5f57e4c3ed60c1eeab81b
liguoying <alinemklee@yeah.net>
2017-08-14 17:55:51 +0800

    activity share animation 细化

--- app/src/main/java/com/androidwebviewdemo/mddemo/SecondActivity.java
+++ app/src/main/java/com/androidwebviewdemo/mddemo/SecondActivity.java
@@ -2,44 +2,54 @@ package com.androidwebviewdemo.mddemo;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Pair;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import com.R;
 
 public class SecondActivity extends AppCompatActivity {
     private Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
     }
-    public void explode(View view){
-        intent = new Intent(this,TransitionsActivity.class);
-        intent.putExtra("flag",0);
+
+    public void explode(View view) {
+        intent = new Intent(this, TransitionsActivity.class);
+        intent.putExtra("flag", 0);
         startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
 
-    public void slide(View view){
-        intent = new Intent(this,TransitionsActivity.class);
-        intent.putExtra("flag",1);
+    public void slide(View view) {
+        intent = new Intent(this, TransitionsActivity.class);
+        intent.putExtra("flag", 1);
         startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
-    public void fade(View view){
-        intent = new Intent(this,TransitionsActivity.class);
-        intent.putExtra("flag",2);
+
+    public void fade(View view) {
+        intent = new Intent(this, TransitionsActivity.class);
+        intent.putExtra("flag", 2);
         startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
-    public void share(View view){
+
+    public void share(View view) {
         View fab = findViewById(R.id.fab_button);
-
-        intent = new Intent(this,TransitionsActivity.class);
-        intent.putExtra("flag",3);
-//        startActivity(intent);
-//        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this,view,"share").toBundle());
-        startActivity(intent,ActivityOptions.makeSceneTransitionAnimation(this, Pair.create(view,"share"),Pair.create(view,"fab")).toBundle());
+        Button button = findViewById(R.id.fab_button);
+        intent = new Intent(this, TransitionsActivity.class);
+        intent.putExtra("flag", 3);
+//        startActivity(intent);/**
+        /**
+         * this: context
+         * view: 共享元素的起始控件
+         * string" 共享元素的标示
+         */
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this, button, "fab").toBundle());
+//        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this, button, "share").toBundle());
+//        startActivity(intent,ActivityOptions.makeSceneTransitionAnimation(this, Pair.create(view,"share"),Pair.create(view,"fab")).toBundle());
     }
 
 }
