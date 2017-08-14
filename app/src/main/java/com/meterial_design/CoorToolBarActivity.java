14b6d684d2eeeab38c50356adea80b354a7447c0
liguoying <alinemklee@yeah.net>
2017-08-14 11:41:07 +0800

    折叠图片  显示titlebar

--- app/src/main/java/com/meterial_design/CoorToolBarActivity.java
+++ app/src/main/java/com/meterial_design/CoorToolBarActivity.java
@@ -2,14 +2,58 @@ package com.meterial_design;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import com.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CoorToolBarActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private CommonRecyclerAdapter<String> mAdapter;
+    private List<String> mStringList;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coor_tool_bar);
+        initViews();
+        initToolBar(mToolbar, false, "");
+        initVariables();
+    }
+
+    private void initVariables() {
+        mStringList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            mStringList.add("测试：" + i);
+        }
+        mAdapter = new CommonRecyclerAdapter<String>(this, mStringList, R.layout.layout_item) {
+            @Override
+            public void convert(CommonRecyclerHolder holder, String item, int position, boolean isScrolling) {
+                holder.setText(R.id.item_text, mStringList.get(position));
+            }
+        };
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+    }
+
+    private void initViews() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+    }
+
+    /**
+     * 初始化 Toolbar
+     */
+    public void initToolBar(Toolbar toolbar, boolean homeAsUpEnabled, String title) {
+        toolbar.setTitle(title);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(homeAsUpEnabled);
     }
 }
