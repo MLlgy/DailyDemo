6c4982da8e9691e8ce45a413843f319160d20c0d
liguoying <alinemklee@yeah.net>
2017-08-14 19:31:26 +0800

    抽屉栏优化

--- app/src/main/java/com/meterial_design/DrawerLayoutActivity.java
+++ app/src/main/java/com/meterial_design/DrawerLayoutActivity.java
@@ -1,13 +1,14 @@
 package com.meterial_design;
 
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import com.R;
+import com.utils.StatusBarUtil;
 
 
 public class DrawerLayoutActivity extends AppCompatActivity {
@@ -19,11 +20,14 @@ public class DrawerLayoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer_layout);
+        DrawerLayout mDrawerLayout = findViewById(R.id.activity_drawer_layout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
 //        actionBar.setHomeAsUpIndicator();
         actionBar.setDisplayHomeAsUpEnabled(true);
+        StatusBarUtil.setTranslucentForDrawerLayout(DrawerLayoutActivity.this, mDrawerLayout, 20);
+        toolbar.setBackgroundColor(getResources().getColor(android.R.color.transparent));
         navigationView = (NavigationView) findViewById(R.id.nav_menu);
         drawerLayout = (DrawerLayout) findViewById(R.id.activity_drawer_layout);
         if(navigationView!=null){
