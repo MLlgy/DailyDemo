9616fd6bc7721443e28ba122402d4d13c4ad14fa
liguoying <alinemklee@yeah.net>
2017-08-28 18:09:30 +0800

    bitmap 绘制文字和图片

--- app/src/main/java/com/CommonActivities/ImageScaleTypeActivity.java
+++ app/src/main/java/com/CommonActivities/ImageScaleTypeActivity.java
@@ -1,15 +1,31 @@
 package com.CommonActivities;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import com.R;
+import com.utils.DrawText;
 
 public class ImageScaleTypeActivity extends AppCompatActivity {
-
+private ImageView mImageView;
+private DrawText mDrawText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_scale_type);
+        initView();
+    }
+
+    private void initView() {
+        mDrawText = new DrawText(this);
+        mImageView = findViewById(R.id.iv_draw_text);
+        mImageView.setImageBitmap(mDrawText
+                .drawTextToBitmap(BitmapFactory
+                        .decodeResource(getResources(),R.mipmap.service_with_zxing),
+                        BitmapFactory
+                                .decodeResource(getResources(),R.mipmap.ic_launcher),
+                        "12344",R.color.black,50,400,200,200));
     }
 }
