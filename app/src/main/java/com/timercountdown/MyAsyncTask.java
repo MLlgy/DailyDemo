e813c9d42d45e6074094b9630a877dc389e7aaa3
liguoying <alinemklee@yeah.net>
2017-09-18 11:12:53 +0800

    add code

--- app/src/main/java/com/timercountdown/MyAsyncTask.java
+++ app/src/main/java/com/timercountdown/MyAsyncTask.java
@@ -1,15 +1,19 @@
 package com.timercountdown;
+
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.TextView;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.widget.TextView;
+
 /**
  * Created by liguoying on 2017/9/9.
  */
 
 public class MyAsyncTask extends AsyncTask<Integer, Integer, String> {
     private static CharSequence mainText = "00.00.00";
+    private static CharSequence strHour = ".00";
     private static CharSequence strSecond = ".00";
     private static CharSequence strMinute = ".00";
     private static CharSequence strMSecond = "00";
@@ -18,6 +22,8 @@ public class MyAsyncTask extends AsyncTask<Integer, Integer, String> {
     private TextView txtView;
     private long number;
 
+    private static final String TAG = "MyAsyncTask";
+    private long hour;
     private long minute;
     private long second;
     private long m_second;
@@ -43,8 +49,9 @@ public class MyAsyncTask extends AsyncTask<Integer, Integer, String> {
     protected void onProgressUpdate(Integer... values) {
 
         super.onProgressUpdate(values);
-
+        Log.e(TAG, "onProgressUpdate: " + number);
         number += number;
+        hour = (((number / 100) / 60) % 60) / 60;
         minute = ((number / 100) / 60) % 60;
         second = (number / 100) % 60;
         m_second = (number % 100);
@@ -61,8 +68,13 @@ public class MyAsyncTask extends AsyncTask<Integer, Integer, String> {
             strMSecond = "0" + m_second;
         else
             strMSecond = "" + m_second;
+        if (hour < 10) {
+            strHour = "0" + hour;
+        } else {
+            strHour = hour + "";
+        }
 
-        mainText = strMinute + "." + strSecond + "." + strMSecond;
+        mainText = hour + "." + strMinute + "." + strSecond + "." + strMSecond;
 
         while (values.equals(1)) {
             number = 0;
