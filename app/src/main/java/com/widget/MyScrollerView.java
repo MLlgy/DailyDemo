794c95b3a67da74c575c1b6e1105c34ce25be908
liguoying <alinemklee@yeah.net>
2017-09-25 11:02:51 +0800

    scrollerview recyclerview 滑动冲突解决

---
+++ app/src/main/java/com/widget/MyScrollerView.java
@@ -0,0 +1,53 @@
+package com.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
+import android.widget.ScrollView;
+
+/**
+ * Created by liguoying on 2017/9/25.
+ */
+
+public class MyScrollerView extends ScrollView {
+    private int downX;
+    private int downY;
+    private int mTouchSlop;
+
+    public MyScrollerView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public MyScrollerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public MyScrollerView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent e) {
+        int action = e.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                downX = (int) e.getRawX();
+                downY = (int) e.getRawY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int moveY = (int) e.getRawY();
+                if (Math.abs(moveY - downY) > mTouchSlop) {
+                    return true;
+                }
+        }
+        return super.onInterceptTouchEvent(e);
+    }
+}
